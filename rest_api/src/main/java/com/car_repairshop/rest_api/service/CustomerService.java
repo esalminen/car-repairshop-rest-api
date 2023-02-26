7656cbf27a16343c34f584588add42f96e0c6c3b
Esa Salminen <esa_salminen@hotmail.com>
2023-02-26 20:37:06 +0200

    added: customer and car crud done

--- rest_api/src/main/java/com/car_repairshop/rest_api/service/CustomerService.java
+++ rest_api/src/main/java/com/car_repairshop/rest_api/service/CustomerService.java
@@ -81,6 +81,21 @@ public class CustomerService {
   }
 
   /**
+   * Returns a car of a customer.
+   */
+  public Car getCarOfCustomerByLicensePlate(Long customerId, String licensePlate) {
+    Customer customer = getCustomerById(customerId);
+    if (customer != null) {
+      for (Car car : customer.getCars()) {
+        if (car.getLicensePlate().equals(licensePlate)) {
+          return car;
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
    * Updates customer data. Returns the updated customer.
    */
   public Customer updateCustomer(Long customerId, Customer updatedCustomer) {
@@ -105,13 +120,8 @@ public class CustomerService {
     Car car = null;
     if (customer != null) {
       for (Car c : customer.getCars()) {
-        if (c.getLicensePlate() == updatedCar.getLicensePlate()) {
-          c.setBrand(updatedCar.getBrand());
-          c.setModel(updatedCar.getModel());
-          c.setYear(updatedCar.getYear());
-          c.setLicensePlate(updatedCar.getLicensePlate());
-          c.setMileage(updatedCar.getMileage());
-          c.setColor(updatedCar.getColor());
+        if (c.getLicensePlate().equals(updatedCar.getLicensePlate())) {
+          c = updatedCar;
           car = c;
         }
       }
@@ -143,7 +153,7 @@ public class CustomerService {
     Car car = null;
     if (customer != null) {
       for (Car c : customer.getCars()) {
-        if (c.getLicensePlate() == licensePlate) {
+        if (c.getLicensePlate().equals(licensePlate)) {
           car = c;
         }
       }
