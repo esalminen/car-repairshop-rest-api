78775aca1b71e337568a8e58314277568d678369
Esa Salminen <esa_salminen@hotmail.com>
2023-03-01 20:36:02 +0200

    updated: service order and -event classes

--- rest_api/src/main/java/com/car_repairshop/rest_api/model/ServiceEvent.java
+++ rest_api/src/main/java/com/car_repairshop/rest_api/model/ServiceEvent.java
@@ -1,5 +1,72 @@
 package com.car_repairshop.rest_api.model;
 
-public class ServiceEvent {
+/**
+ * This class represents a service event for reserved car maintenance.
+ * 
+ * @author Esa Salminen
+ * @version 1.0
+ * @since 1.3.2023
+ * 
+ * 
+ *        Service event is a completed work of a car maintenance. It is created
+ *        by repairshop worker and it is linked to a service order. Service
+ *        event has a description of the work done and the time it took to
+ *        complete ordered work.
+ */
 
+public class ServiceEvent {
+  private Long id;
+  private Long serviceOrderId;
+  private Long workerId;
+  private String serviceDescription;
+  private int workHours;
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getServiceOrderId() {
+    return this.serviceOrderId;
+  }
+
+  public void setServiceOrderId(Long serviceOrderId) {
+    this.serviceOrderId = serviceOrderId;
+  }
+
+  public Long getWorkerId() {
+    return this.workerId;
+  }
+
+  public void setWorkerId(Long workerId) {
+    this.workerId = workerId;
+  }
+
+  public String getServiceDescription() {
+    return this.serviceDescription;
+  }
+
+  public void setServiceDescription(String serviceDescription) {
+    this.serviceDescription = serviceDescription;
+  }
+
+  public int getWorkHours() {
+    return this.workHours;
+  }
+
+  public void setWorkHours(int workHours) {
+    this.workHours = workHours;
+  }
+
+  public ServiceEvent() {
+  }
+
+  public ServiceEvent(Long serviceOrderId, String serviceDescription, int workHours) {
+    this.serviceOrderId = serviceOrderId;
+    this.serviceDescription = serviceDescription;
+    this.workHours = workHours;
+  }
 }
--- rest_api/src/main/java/com/car_repairshop/rest_api/model/ServiceOrder.java
+++ rest_api/src/main/java/com/car_repairshop/rest_api/model/ServiceOrder.java
@@ -1,5 +1,27 @@
 package com.car_repairshop.rest_api.model;
 
-public class ServiceOrder {
+/**
+ * This class represents a service order for car maintenance.
+ * 
+ * @author Esa Salminen
+ * @version 1.0
+ * @since 1.3.2023
+ * 
+ * 
+ *        Service order is a reservation for car maintenance in the repair shop.
+ *        It is created by customer service and it is linked to a car and a
+ *        customer. Service order has a description of the work to be done.
+ *        Service order is linked to a service event when the work is completed.
+ *        Price of the service order is calculated from the service event based
+ *        on worker's charge per hour and work hours. Service order is paid when
+ *        the customer pays the bill.
+ */
 
+public class ServiceOrder {
+  private Long id;
+  private Long customerId;
+  private Long carLisencePlate;
+  private ServiceEvent serviceEvent;
+  private String serviceOrderDescription;
+  private boolean serviceOrderPaid;
 }
